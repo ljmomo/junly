d435f57cce07fe6fd42df99d27f627cf2dc9c8c9
mokingone <lijunok1314@163.com>
2015-07-01 09:50:18 +0800

    使用spring的注解的声明式事务。管理。

--- src/com/it/junly/service/impl/UserServiceImpl.java
+++ src/com/it/junly/service/impl/UserServiceImpl.java
@@ -3,6 +3,8 @@ package com.it.junly.service.impl;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.it.junly.mapper.UserMapper;
 import com.it.junly.po.User;
@@ -12,6 +14,7 @@ public class UserServiceImpl implements UserService {
 	@Resource
 	private UserMapper userMapper;
 	@Override
+	@Transactional(propagation=Propagation.SUPPORTS,readOnly=true)
 	public User getUserById(int id) throws Exception {
 		return userMapper.selectUserById(id);
 	}
